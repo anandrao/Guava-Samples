fe4d4a397a0f2ca19476d1b8ac834fa3f1833e37
Anand Rao <anand.r.rao@gmail.com>
2012-02-12 16:13:33 -0600

    Added some CharMatcher test cases and CaseFormatter test cases.

--- src/test/java/com/guava/StringUtilitiesTest.java
+++ src/test/java/com/guava/StringUtilitiesTest.java
@@ -4,21 +4,56 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 
 public class StringUtilitiesTest {
 
 	@Test
-	public void test_BasicJoinerSkippingNulls() {
+	public void test_Basic_Joiner_SkippingNulls() {
 		Joiner joiner = Joiner.on("; ").skipNulls();
 		assertEquals("Harry; Ron; Hermione", joiner.join("Harry", null, "Ron", "Hermione"));
 	}
 	
 	@Test
-	public void test_SplitterChar(){
+	public void test_Splitter_Char(){
 		Joiner joiner = Joiner.on("; ");
 		assertEquals("Harry; Ron; Hermione",joiner.join(Splitter.on('x').split("HarryxRonxHermione")));
+	}
+	
+	@Test
+	public void test_Splitter_CharMatcher(){
+		Joiner joiner = Joiner.on("; ");
+		assertEquals("Harry; Ron; Hermione",joiner.join(Splitter.on(CharMatcher.anyOf("0O")).split("HarryORon0Hermione")));
+	}
+	
+	@Test
+	public void test_Splitter_CharMatcher_OmitEmptyStrings(){
+		Joiner joiner = Joiner.on("; ");
+		assertEquals("Harry; Ron; Hermione",joiner.join(Splitter.on(CharMatcher.anyOf("0O")).omitEmptyStrings().split("HarryO000Ron0Hermione")));
+	}
+	
+	@Test
+	public void test_Splitter_CharMatcher_OmitEmptyStringsWhitespace(){
+		Joiner joiner = Joiner.on("; ");
+		assertEquals("Harry; Ron; Hermione",joiner.join(Splitter.on(CharMatcher.anyOf("0O")).omitEmptyStrings().trimResults().split("Harry  O000Ron0Hermione")));
+	}
+	
+	@Test
+	public void test_CharMatcher_MatchesAll(){
+		assertTrue(CharMatcher.ASCII.matchesAllOf("kjsdhnfkjncksadjfh"));
+	}
+	
+	@Test
+	public void test_CharMatcher_CollapseFrom(){
+		assertEquals("loremxipsumxdolor", CharMatcher.WHITESPACE.collapseFrom("lorem ipsum dolor", 'x'));
+	}
+	
+	@Test
+	public void test_CaseFormat(){
+		assertEquals("constantName",CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "CONSTANT_NAME"));
 	}
 
 }
