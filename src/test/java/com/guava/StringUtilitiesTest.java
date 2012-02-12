5dbf543c36abad0fa2a2da4aaca51fc2e79b68d2
Anand Rao <anand.r.rao@gmail.com>
2012-02-12 14:53:04 -0600

    Added Joiner and Splitter tests.

--- src/test/java/com/guava/StringUtilitiesTest.java
+++ src/test/java/com/guava/StringUtilitiesTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+
 public class StringUtilitiesTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_BasicJoinerSkippingNulls() {
+		Joiner joiner = Joiner.on("; ").skipNulls();
+		assertEquals("Harry; Ron; Hermione", joiner.join("Harry", null, "Ron", "Hermione"));
+	}
+	
+	@Test
+	public void test_SplitterChar(){
+		Joiner joiner = Joiner.on("; ");
+		assertEquals("Harry; Ron; Hermione",joiner.join(Splitter.on('x').split("HarryxRonxHermione")));
 	}
 
 }
